08a867c19826853aca6c641824f05bd55162f2d7
serhioms <sergey.moskovskiy@gmail.com>
2021-02-11 16:28:20 -0500

    LMax disruptor test

--- Interview/src/main/java/ca/interview/rbc/transactions/NotificationManager.java
+++ Interview/src/main/java/ca/interview/rbc/transactions/NotificationManager.java
@@ -19,6 +19,7 @@ public class NotificationManager {
 		// Remove expired transaction from the head
 		if( list.size() == 0 ) {
 		} else if( list.getLast().expiredMls < startTransaction ) {
+			transaction.numExpired += list.size(); // for print
 			list.clear();
 			accSum = 0;
 		} else {
