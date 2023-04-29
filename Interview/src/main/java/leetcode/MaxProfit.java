04bd9f4a3ee3b92ec20ff2296e60c0a068f1d0b5
serhioms <sergey.moskovskiy@gmail.com>
2023-04-29 00:15:20 -0400

    Fucking Genius #1 git add --all!

--- Interview/src/main/java/leetcode/MaxProfit.java
+++ Interview/src/main/java/leetcode/MaxProfit.java
@@ -13,8 +13,8 @@ public class MaxProfit {
 
     public int maxProfitMyBrut(int[] prices) {
         int maxProfit = 0;
-        for(int buy=0,max=prices.length; buy<max-1; ++buy){
-            for(int sell=buy+1; sell < max; ++sell){
+        for (int buy = 0, max = prices.length; buy < max - 1; ++buy) {
+            for (int sell = buy + 1; sell < max; ++sell) {
                 int profit = prices[sell] - prices[buy];
                 maxProfit = Math.max(maxProfit, profit);
             }
@@ -25,20 +25,20 @@ public class MaxProfit {
     // 15.88% Memory 59.5 MB Beats 20.35%
     public int maxProfit3My(int[] prices) {
         int a = 0;
-        int b = prices.length-1;
+        int b = prices.length - 1;
         int mina = prices[a];
         int maxa = prices[a];
         int maxb = prices[b];
         int minb = prices[b];
         int maxProfit = prices[b] - prices[a];
-        while( ++a <= --b){
-            maxa = Math.max(maxa,prices[a]);
-            minb = Math.min(minb,prices[b]);
-            if( prices[a] < mina ){
+        while (++a <= --b) {
+            maxa = Math.max(maxa, prices[a]);
+            minb = Math.min(minb, prices[b]);
+            if (prices[a] < mina) {
                 maxProfit = Math.max(maxProfit, maxa - mina);
                 mina = maxa = prices[a];
             }
-            if( prices[b] > maxb ){
+            if (prices[b] > maxb) {
                 maxProfit = Math.max(maxProfit, maxb - minb);
                 maxb = minb = prices[b];
             }
@@ -65,70 +65,132 @@ public class MaxProfit {
         }*/
         int res = Integer.MIN_VALUE;
 
-        for(int i = prices.length - 1; i >= 1; i--)
-            for(int j = i - 1;j >= 0; j--)
-                if((prices[i] - prices[j]) >= res)
+        for (int i = prices.length - 1; i >= 1; i--)
+            for (int j = i - 1; j >= 0; j--)
+                if ((prices[i] - prices[j]) >= res)
                     res = prices[i] - prices[j];
 
-        if(res < 0) return 0;
+        if (res < 0) return 0;
         return res;
     }
 
     public int maxProfit1(int[] prices) {
         int minsofar = prices[0];
         int maxprofit = 0;
-        for(int price: prices){
+        for (int price : prices) {
             minsofar = Math.min(minsofar, price);
             maxprofit = Math.max(maxprofit, price - minsofar);
         }
         return maxprofit;
     }
 
+    // Save on memory due to no stack operations
+    public int maxProfit4(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+
+    public int maxProfit5(int[] prices) {
+        int a = 0, b = 1;
+        int max = Integer.MIN_VALUE;
+        while (b < prices.length) {
+            int profit = prices[b] - prices[a];
+            if (prices[b] > prices[a]) {
+                if (profit > max) {
+                    max = profit;
+                }
+                ++b;
+            } else {
+                a = b;
+                ++b;
+            }
+        }
+        if (max < 0) {
+            return 0;
+        }
+        return max;
+    }
+
+    public int maxProfit6(int[] prices) {
+        int a = 0;
+        int b = 1;
+        int maxProfit = 0;
+        int cProfit = 0;
+        while (b < prices.length) {
+            if (prices[a] < prices[b]) {
+                cProfit = prices[b] - prices[a];
+                if (maxProfit < cProfit) {
+                    maxProfit = cProfit;
+                }
+            } else {
+                a = b;
+            }
+            ++b;
+        }
+        return maxProfit;
+    }
+
     public int maxProfit2(int[] prices) {
         int n = prices.length;
-        int [] aux = new int[n];
+        int[] aux = new int[n];
         int maxi = 0;
         int maxProfit = 0;
-
         // In this appproach i am creating an array which will contain the maximum price that the stock will attain in fure
-        for(int i = n-1; i>=0; --i){
-            maxi = Math.max(maxi, prices[i]);
+        for (int i = n - 1; i >= 0; --i) {
+            if (prices[i] > maxi) {
+                maxi = prices[i];
+            }
             aux[i] = maxi;
         }
-
-        for(int i = 0; i<n; ++i){
+        for (int i = 0; i < n; ++i) {
             //we  will sell the stock each day now and store it in profit
-            int   profit = aux[i] - prices[i];
+            int profit = aux[i] - prices[i];
             // we will now compare the profit of each day and update the maximum profit in maxProfit
-            maxProfit =  Math.max(maxProfit, profit);
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
         }
-
         return maxProfit;
     }
 
     @Test
-    public void testMaxProfitA(){
-        int[] A = new int[]{7,1,5,3,6,4};
+    public void testMaxProfitA() {
+        int[] A = new int[]{7, 1, 5, 3, 6, 4};
         assertEquals(5, maxProfitMyBrut(A));
-        assertEquals(5, maxProfit3My(A));
         assertEquals(5, maxProfit0(A));
         assertEquals(5, maxProfit1(A));
         assertEquals(5, maxProfit2(A));
-     }
-
-    @Test
-    public void testMaxProfitB(){
-        int[] B = new int[]{3,2,6,5,0,3};
-        assertEquals(4, maxProfitMyBrut(B));
-        assertEquals(4, maxProfit3My(B));
-        assertEquals(4, maxProfit0(B));
-        assertEquals(4, maxProfit1(B));
-        assertEquals(4, maxProfit2(B));
+        assertEquals(5, maxProfit3My(A));
+        assertEquals(5, maxProfit4(A));
+        assertEquals(5, maxProfit5(A));
+        assertEquals(5, maxProfit6(A));
     }
 
     @Test
-    public void testMaxProfitC() throws Exception{
+    public void testMaxProfitB() {
+        int[] B = new int[]{3, 2, 6, 5, 0, 3};
+        assertEquals(4, maxProfitMyBrut(B));
+        assertEquals(4, maxProfit0(B));
+        assertEquals(4, maxProfit1(B));
+        assertEquals(4, maxProfit2(B));
+        assertEquals(4, maxProfit3My(B));
+        assertEquals(4, maxProfit4(B));
+        assertEquals(4, maxProfit5(B));
+        assertEquals(4, maxProfit5(B));
+    }
+
+    @Test
+    public void testMaxProfitC() throws Exception {
         int[] C = readIntArray("data/maxprofit.txt");
+
         long start = System.nanoTime();
         assertEquals(999, maxProfitMyBrut(C));
         long end = System.nanoTime();
@@ -149,19 +211,34 @@ public class MaxProfit {
         assertEquals(999, maxProfit2(C));
         long end2 = System.nanoTime();
 
+        long start4 = System.nanoTime();
+        assertEquals(999, maxProfit4(C));
+        long end4 = System.nanoTime();
+
+        long start5 = System.nanoTime();
+        assertEquals(999, maxProfit5(C));
+        long end5 = System.nanoTime();
+
+        long start6 = System.nanoTime();
+        assertEquals(999, maxProfit6(C));
+        long end6 = System.nanoTime();
+
         System.out.printf("   My brut, nano = %,d\n", end - start);
-        System.out.printf("      My 3, nano = %,d\n", end3 - start3);
-        System.out.printf("  Genius 2, nano = %,d\n", end3 - start3);
-        System.out.printf("  Genius 1, nano = %,d\n", end1 - start1);
+        System.out.printf("       My 3, nano = %,d\n", end3 - start3);
+        System.out.printf("   Genius 2, nano = %,d no Math.min/max !\n", end2 - start2);
+        System.out.printf("  Genius 14, nano = %,d\n", end1 - start1);
         System.out.printf("'Genius 0', nano = %,d\n", end0 - start0);
-     }
+        System.out.printf("  Genius 41, nano = %,d no Math.min/max !!!\n", end4 - start4);
+        System.out.printf("   Genius 5, nano = %,d no Math.min/max\n", end5 - start5);
+        System.out.printf("   Genius 6, nano = %,d no Math.min/max !!\n", end6 - start6);
+    }
 
     private int[] readIntArray(String filePath) throws Exception {
         String[] strarr = null;
-        InputStream in =  new FileInputStream(filePath);
-        if( in != null ){
+        InputStream in = new FileInputStream(filePath);
+        if (in != null) {
             Scanner scanner = new Scanner(in);
-            while(scanner.hasNext()){
+            while (scanner.hasNext()) {
                 strarr = scanner.next().split(",");
             }
         }
