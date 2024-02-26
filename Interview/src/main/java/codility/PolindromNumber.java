435904c0a94f503077f1c9c33907d0033303296e
Sergey Moskovskiy <sergey.moskovskiy@priceline.com>
2024-02-26 15:58:28 -0500

    New codility for bns

--- Interview/src/main/java/codility/PolindromNumber.java
+++ Interview/src/main/java/codility/PolindromNumber.java
@@ -13,7 +13,7 @@ public class PolindromNumber {
 		}
 		String left = "";
 		String right = "";
-		for(int i=9; i>=0; --i){
+		for(int i=9; i>0; --i){
 			while( cnt[i] > 0 && cnt[i] % 2 == 0){
 				left = left + i;
 				right = i + right;
@@ -37,9 +37,10 @@ public class PolindromNumber {
 	@Test
 	public void testMaxPolinfromNumber() {
 		assertEquals(-1, solution(new int[]{}));
-		assertEquals(898, solution(new int[]{1,7,8,3,9,8}));
-		assertEquals(5, solution(new int[]{1,2,3,4,5}));
-		assertEquals(7, solution(new int[]{0,0,0,7}));
+		assertEquals(898, solution(new int[]{3,9,8,7,8}));
+		assertEquals(0, solution(new int[]{0,0,0,0}));
+		assertEquals(5, solution(new int[]{5,4,3,2,1}));
+		assertEquals(9, solution(new int[]{0,9,0}));
 		assertEquals(99899, solution(new int[]{1,2,3,4,5,6,7,8,9,9,9,9}));
 		assertEquals(9987899, solution(new int[]{1,2,3,4,5,6,7,8,8,9,9,9,9}));
 		assertEquals(99099, solution(new int[]{0,9,9,9,9}));
