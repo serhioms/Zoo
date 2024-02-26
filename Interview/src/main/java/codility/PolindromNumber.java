553b6ae1b9e16be5065ca1f0917bfc1040c58967
Sergey Moskovskiy <sergey.moskovskiy@priceline.com>
2024-02-26 15:55:50 -0500

    New codility for bns

---
+++ Interview/src/main/java/codility/PolindromNumber.java
@@ -0,0 +1,47 @@
+package codility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PolindromNumber {
+
+	public int solution(int[] S) {
+		int[] cnt = new int[10];
+		for(int i: S){
+			++cnt[i];
+		}
+		String left = "";
+		String right = "";
+		for(int i=9; i>=0; --i){
+			while( cnt[i] > 0 && cnt[i] % 2 == 0){
+				left = left + i;
+				right = i + right;
+				cnt[i] -= 2;
+			}
+		}
+		String result = null;
+		for(int i=9; i>=0; --i){
+			if( cnt[i] > 0 ){
+				result = left+i+right;
+				break;
+			}
+		}
+		if( result == null ){
+			result = left+right;
+		}
+		return result.isEmpty()? -1: Integer.parseInt(result);
+	}
+
+
+	@Test
+	public void testMaxPolinfromNumber() {
+		assertEquals(-1, solution(new int[]{}));
+		assertEquals(898, solution(new int[]{1,7,8,3,9,8}));
+		assertEquals(5, solution(new int[]{1,2,3,4,5}));
+		assertEquals(7, solution(new int[]{0,0,0,7}));
+		assertEquals(99899, solution(new int[]{1,2,3,4,5,6,7,8,9,9,9,9}));
+		assertEquals(9987899, solution(new int[]{1,2,3,4,5,6,7,8,8,9,9,9,9}));
+		assertEquals(99099, solution(new int[]{0,9,9,9,9}));
+	}
+}
